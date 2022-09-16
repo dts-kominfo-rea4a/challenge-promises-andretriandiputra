396e60a2cfc438ca55ec6165869384b512a39f9d
andretriandiputra <andretriandiputra@gmail.com>
2022-09-16 08:33:09 +0700

    Task promiseOutput function

--- promise.js
+++ promise.js
@@ -1,10 +1,10 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = async (hasil) => {
+const promiseOutput = async (checkHasil) => {
   try{
-    const hasilIXX = await promiseTheaterIXX().then(data => data.filter((film) => film.hasil == hasil).length);
-    const hasilVGC = await promiseTheaterVGC().then(data => data.filter((film) => film.hasil == hasil).length);
+    const hasilIXX = await promiseTheaterIXX().then(data => data.filter((film) => film.hasil == checkHasil).length);
+    const hasilVGC = await promiseTheaterVGC().then(data => data.filter((film) => film.hasil == checkHasil).length);
     return hasilIXX + hasilVGC;
   } catch (err) {
     console.log(err);
